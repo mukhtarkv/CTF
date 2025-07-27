baf7ea6b8a1e32251ca22baa3e006b699247c0f1
Marc <marc@farm360.ai>
2025-07-27 13:55:44 +0900

    feat(BE): use key value storage for global state

--- backend/src/hello.rs
+++ backend/src/hello.rs
@@ -1,13 +1,14 @@
+use crate::state::SharedState;
 use axum::{
     Router,
-    extract::Query,
+    extract::{Query, State},
     response::{Html, IntoResponse},
     routing::get,
 };
 use serde::Deserialize;
 
 // NOTE: just for testing
-pub fn routes_hello() -> Router {
+pub fn routes_hello() -> Router<SharedState> {
     Router::new()
         .route("/good-bye", get(handler_good_bye))
         .route("/hello/{name}", get(handler_hello))
@@ -24,7 +25,9 @@ async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     Html(format!("Hello <strong>{name}</strong>"))
 }
 
-async fn handler_good_bye() -> impl IntoResponse {
-    println!("->> {:<12} - handler_good_bye", "HANDLER");
+async fn handler_good_bye(State(state): State<SharedState>) -> impl IntoResponse {
+    // Acquire read lock and debug-print the entire AppState
+    let guard = state.read().unwrap();
+    println!("Current AppState: {:?}", *guard);
     Html(format!("Good bye!"))
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,62 @@
-use axum::Router;
-use tokio::net::TcpListener;
-
-use crate::hello::routes_hello;
-
 pub use self::error::{Error, Result};
+use crate::{hello::routes_hello, state_handler::routes_kv};
+use axum::{Router, error_handling::HandleErrorLayer, http::StatusCode, response::IntoResponse};
+use std::{borrow::Cow, sync::Arc, time::Duration};
+use tokio::net::TcpListener;
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod error;
 mod hello;
+mod state;
+mod state_handler;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let routes_all = Router::new().merge(routes_hello());
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let shared_state = state::SharedState::default();
+    let app = Router::new()
+        // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                // Handle errors from middleware
+                .layer(HandleErrorLayer::new(handle_error))
+                .load_shed()
+                .concurrency_limit(1024)
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http()),
+        )
+        .merge(routes_hello())
+        .merge(routes_kv())
+        .with_state(Arc::clone(&shared_state));
 
     let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
-    axum::serve(listener, routes_all.into_make_service())
-        .await
-        .unwrap();
-
-    Ok(())
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
 
+async fn handle_error(error: BoxError) -> impl IntoResponse {
+    if error.is::<tower::timeout::error::Elapsed>() {
+        return (StatusCode::REQUEST_TIMEOUT, Cow::from("request timed out"));
+    }
+
+    if error.is::<tower::load_shed::error::Overloaded>() {
+        return (
+            StatusCode::SERVICE_UNAVAILABLE,
+            Cow::from("service is overloaded, try again later"),
+        );
+    }
+
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        Cow::from(format!("Unhandled internal error: {error}")),
+    )
+}
